373e6f90705c9cc097c00cba34b5f07893935eef
willcod <william.wow@icloud.com>
2021-09-09 11:26:10 +0800

    Pub-Sub pattern

--- WpfExample/MainWindow.xaml.cs
+++ WpfExample/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfExample
 {
@@ -22,6 +13,55 @@ namespace WpfExample
     {
         public MainWindow() {
             InitializeComponent();
+
+            (new Thread(new ThreadStart(ServerThreadFunction))).Start();
+            (new Thread(new ThreadStart(ClientAThreadFunction))).Start();
+            (new Thread(new ThreadStart(ClientBThreadFunction))).Start();
+        }
+
+        private void ServerThreadFunction() {
+            PublisherSocket puber = new();
+            puber.Bind("tcp://*:5556");
+
+            while (true) {
+                puber.SendMoreFrame("TopicA").SendFrame("alarm from A");
+                Dispatcher.BeginInvoke(() => ServerList.Items.Add("Send alarm from A"));
+                puber.SendMoreFrame("TopicB").SendFrame("alarm from B");
+
+                Dispatcher.BeginInvoke(() => ServerList.Items.Add("Send alarm from B"));
+
+                Thread.Sleep(500);
+            }
+        }
+
+        private void ClientAThreadFunction() {
+            SubscriberSocket suber = new();
+            suber.Connect("tcp://localhost:5556");
+            suber.Subscribe("TopicA");
+
+            while (true) {
+                string topic = suber.ReceiveFrameString();
+                string content = suber.ReceiveFrameString();
+
+                Dispatcher.BeginInvoke(() => ClientAList.Items.Add("receive: " + content));
+
+                Thread.Sleep(500);
+            }
+        }
+
+        private void ClientBThreadFunction() {
+            SubscriberSocket suber = new();
+            suber.Connect("tcp://localhost:5556");
+            suber.Subscribe("TopicB");
+
+            while (true) {
+                string topic = suber.ReceiveFrameString();
+                string content = suber.ReceiveFrameString();
+
+                Dispatcher.BeginInvoke(() => ClientBList.Items.Add("receive: "+ content));
+
+                Thread.Sleep(500);
+            }
         }
     }
 }
